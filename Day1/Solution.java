954b54edac63e3525ced3d253afff88252c20a56
maibu pashh <71766168+miabu-pashh@users.noreply.github.com>
2023-04-17 14:32:16 +0000

    demo

---
+++ Day1/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public Class Solution{
+    public static void main(String []args){
+        ArrayList<Integer> calories=new ArrayList<Integer>();
+        int maxSum=0;
+        try{
+            Scanner sc=new Scanner(new File("Day1/input.txt"));
+            while(sc.hasNext()){
+                String line=
+                if(sc.equals("")){
+
+                }
+            }
+        }
+
+    }
+
+}
